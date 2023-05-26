5e0020d2a0bb210d10df2f237ffce2994975b4bc
itumejia <iturielmg@hotmail.com>
2023-05-25 23:11:31 -0600

    fix

--- codigo_alta_similitud.java
+++ codigo_alta_similitud.java
@@ -1,56 +1,67 @@
-public class MergeSort {
-    public static void mergeSort(int[] array) {
-        if (array == null || array.length <= 1) {
-            return; // Already sorted
-        }
-        
-        int mid = array.length / 2;
-        int[] left = new int[mid];
-        int[] right = new int[array.length - mid];
-        
-        // Split the array into two halves
-        System.arraycopy(array, 0, left, 0, left.length);
-        System.arraycopy(array, mid, right, 0, right.length);
-        
-        // Recursively sort the two halves
-        mergeSort(left);
-        mergeSort(right);
-        
-        // Merge the sorted halves
-        merge(left, right, array);
+import java.util.List;
+
+public class Coche {
+    private boolean motorEncendido;
+    private int velocidadActual;
+    private int carrilActual;
+
+    public Coche() {
+        motorEncendido = false;
+        velocidadActual = 0;
+        carrilActual = 1;
     }
-    
-    private static void merge(int[] left, int[] right, int[] result) {
-        int i = 0; // Index for the left array
-        int j = 0; // Index for the right array
-        int k = 0; // Index for the result array
-        
-        while (i < left.length && j < right.length) {
-            if (left[i] <= right[j]) {
-                result[k++] = left[i++];
-            } else {
-                result[k++] = right[j++];
-            }
-        }
-        
-        // Copy remaining elements from the left array, if any
-        while (i < left.length) {
-            result[k++] = left[i++];
-        }
-        
-        // Copy remaining elements from the right array, if any
-        while (j < right.length) {
-            result[k++] = right[j++];
+
+    public void iniciarMotor() {
+        if (!motorEncendido) {
+            motorEncendido = true;
+            System.out.println("El motor se ha encendido.");
+        } else {
+            System.out.println("El motor ya está encendido.");
         }
     }
-    
+
+    public void apagarMotor() {
+        if (motorEncendido) {
+            motorEncendido = false;
+            System.out.println("El motor se ha apagado.");
+        } else {
+            System.out.println("El motor ya está apagado.");
+        }
+    }
+
+    public void aumentarVelocidad() {
+        if (motorEncendido) {
+            velocidadActual += 10;
+            System.out.println("El coche ha aumentado la velocidad. Velocidad actual: " + velocidadActual + " km/h.");
+        } else {
+            System.out.println("El motor está apagado. No se puede aumentar la velocidad.");
+        }
+    }
+
+    public void reducirVelocidad() {
+        if (velocidadActual > 0) {
+            velocidadActual -= 10;
+            System.out.println("El coche ha reducido la velocidad. Velocidad actual: " + velocidadActual + " km/h.");
+        } else {
+            System.out.println("El coche ya está detenido.");
+        }
+    }
+
+    public void cambiarCarril(int nuevoCarril) {
+        if (motorEncendido) {
+            carrilActual = nuevoCarril;
+            System.out.println("El coche ha cambiado al carril " + carrilActual + ".");
+        } else {
+            System.out.println("El motor está apagado. No se puede cambiar de carril.");
+        }
+    }
+
     public static void main(String[] args) {
-        int[] array = {9, 5, 1, 3, 8, 4, 2, 7, 6};
-        
-        System.out.println("Original array: " + Arrays.toString(array));
-        
-        mergeSort(array);
-        
-        System.out.println("Sorted array: " + Arrays.toString(array));
+        Coche miCoche = new Coche();
+        miCoche.iniciarMotor();
+        miCoche.aumentarVelocidad();
+        miCoche.cambiarCarril(2);
+        miCoche.reducirVelocidad();
+        miCoche.apagarMotor();
     }
 }
